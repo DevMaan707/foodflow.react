bfa2ea6a98ada8d26869585aa75f034e123ed1c5
cyriljohn147 <cyriljohn142005@gmail.com>
2025-06-19 11:28:18 +0530

    added api integration to backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
+import ReceiverDashboard from "./components/RecieverDashboard";
+// import AdminDashboard from "./components/AdminDashboard";
 
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind!</h1>
-    </div>
-  );
+  return <ReceiverDashboard />;
 }
 export default App;
